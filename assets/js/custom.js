6fc2a87ff314318bfd7389902ef8044b6e73cef5
Bayu <bayu.setiaji709@gmail.com>
2021-10-31 20:10:01 +0700

    problem-retrivingData

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,31 @@
-const btn = document.getElementById('submit');
-btn.addEventListener('click', function(){
-    Swal.fire({
-        icon: 'success',
-        title: 'Success Send Message',
-        text: 'Message Sent to Alterra',
-    })
+// Validation with Bootstrap
+var forms = document.querySelectorAll(".needs-validation");
+Array.prototype.slice.call(forms).forEach(function(form) {
+    form.addEventListener("submit", function(event) {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+    }, false);
 });
+
+// Local Storage 
+const localVal = () => {
+    let fName = document.getElementById("fName").value;
+    let email = document.getElementById("email").value;
+    let phoneNumber = document.getElementById("phoneNumber").value;
+    let nationality = document.getElementById("nationality").value;
+    let message = document.getElementById("messageText").value;
+    const messages = [
+        { fullName: fName, emails: email, phone: phoneNumber, national: nationality, pesan: message }
+    ];
+    localStorage.setItem("response", JSON.stringify(messages));
+
+    // localStorage.setItem("fullname", fullname);
+    // localStorage.setItem("email", emails);
+    // localStorage.setItem("phone", phones);
+    // localStorage.setItem("nationals", nationals);
+    // localStorage.setItem("messages", messages);
+    return false;
+};
